811b8ff88b60a84da53db3dd4dfce415cd90c078
nikitamakarchuk <kukushkanezabudka@yandex.by>
2021-12-13 12:57:48 +0300

    added fool check for cli

--- config/cli.go
+++ config/cli.go
@@ -5,6 +5,7 @@ import (
 	"github.com/bykovme/goconfig"
 	"github.com/google/uuid"
 	"log"
+	"strings"
 )
 
 const welcomeText = "informer.conf is not found when starting the application," +
@@ -15,20 +16,32 @@ func startCli(usrHomePath string) (createdConfig *Config, err error) {
 
 	createdConfig = new(Config)
 
-	fmt.Println("input ticker: ")
-	fmt.Scan(&createdConfig.Ticker)
+	for isInputEmpty(createdConfig.Ticker) {
+		fmt.Println("input ticker: ")
+		fmt.Scan(&createdConfig.Ticker)
+	}
+
 	fmt.Println("input location: ")
 	fmt.Scan(&createdConfig.Location)
-	fmt.Println("input controller_url: ")
-	fmt.Scan(&createdConfig.ControllerURL)
-	fmt.Println("input node_monitoring url: ")
-	fmt.Scan(&createdConfig.NodeMonitoringURL)
-	fmt.Println("input time_for_frequently_update: ")
-	fmt.Scan(&createdConfig.TimeForFrequentlyUpdate)
-	fmt.Println("input time_for_rare_update: ")
-	fmt.Scan(&createdConfig.TimeForRareUpdate)
+
+	for isInputEmpty(createdConfig.ControllerURL) {
+		fmt.Println("input controller_url: ")
+		fmt.Scan(&createdConfig.ControllerURL)
+	}
+
+	for isInputEmpty(createdConfig.NodeMonitoringURL) {
+		fmt.Println("input node_monitoring url: ")
+		fmt.Scan(&createdConfig.NodeMonitoringURL)
+	}
+
+	createdConfig.TimeForFrequentlyUpdate = 10
+	createdConfig.TimeForRareUpdate = 60
+
 	fmt.Println("input blockchain: ")
 	fmt.Scan(&createdConfig.Blockchain)
+	if isInputEmpty(createdConfig.Blockchain) {
+		createdConfig.Blockchain = "cardano"
+	}
 
 	createdConfig.UUID = uuid.New().String()
 	fmt.Println("Your uuid:", createdConfig.UUID)
@@ -41,4 +54,10 @@ func startCli(usrHomePath string) (createdConfig *Config, err error) {
 
 	fmt.Println("config path:", usrHomePath+cConfigPath)
 	return createdConfig, nil
+}
+
+func isInputEmpty(input string) bool {
+	input = strings.ReplaceAll(input, " ", "")
+
+	return len(input) == 0
 }

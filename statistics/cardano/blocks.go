7036da5365a4b875059abd245123d67ff381b62b
Sergey Shpilevskiy <siarhei.shpileuski@gmail.com>
2021-12-15 11:15:50 +0300

    Update blocks.go

--- statistics/cardano/blocks.go
+++ statistics/cardano/blocks.go
@@ -9,7 +9,7 @@ import (
 // GetBlocks -
 func (cardano *Cardano) GetBlocks(jsonBody string) *pb.Blocks {
 	var blocks pb.Blocks
-	blocks.BlockLeader = gjson.Get(jsonBody, "cardano.node.metrics.Forge[\"node-is-leader\"].int.val").Int()
+	blocks.BlockLeader = gjson.Get(jsonBody, "cardano.node.metrics.Forge.node-is-leader.int.val").Int()
 	blocks.BlockAdopted = gjson.Get(jsonBody, "cardano.node.metrics.Forge.adopted.int.val").Int()
 	blocks.BlockInvalid = gjson.Get(jsonBody, "cardano.node.metrics.Forge[\"didnt-adopt\"].int.val").Int()
 	return &blocks

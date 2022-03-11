443fdde71ca25acc57781e7ffee84aa3de8e19f9
Nikita <kukushkanezabudka@yandex.by>
2022-03-11 15:10:34 +0300

    Br 33 add more statistic (#6)
    * added cpu statistic
    
    * added check for new versions of informer on GitHub

--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	github.com/bykovme/goconfig v0.0.0-20170717154220-caa70d3abfca
 	github.com/google/uuid v1.1.2
 	github.com/sergey-shpilevskiy/go-bytesize v1.0.0
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/tidwall/gjson v1.9.3
+	golang.org/x/sys v0.0.0-20220310020820-b874c991c1a5 // indirect
 	google.golang.org/grpc v1.37.1
 )
--- statistics/common/cpu.go
+++ statistics/common/cpu.go
@@ -1,13 +1,53 @@
 package common
 
-import pb "github.com/adarocket/proto/proto-gen/common"
+import (
+	pb "github.com/adarocket/proto/proto-gen/common"
+	"golang.org/x/sys/unix"
+	"log"
+	"runtime"
+	"unsafe"
+)
 
 // GetCPUState -
 func (commonStatistic *CommonStatistic) GetCPUState() *pb.CPUState {
 	var cpuState pb.CPUState
 
-	cpuState.CpuQty = 1
-	cpuState.AverageWorkload = 0.0
+	cpuState.CpuQty = int64(runtime.NumCPU())
+	load, err := avgLoad()
+	if err != nil {
+		log.Println(err)
+	}
+
+	cpuState.AverageWorkload = float32(load.Load15)
 
 	return &cpuState
+}
+
+type avgStat struct {
+	Load1  float64
+	Load5  float64
+	Load15 float64
+}
+
+func avgLoad() (avgStat, error) {
+	type loadavg struct {
+		load  [3]uint32
+		scale int
+	}
+
+	b, err := unix.SysctlRaw("vm.loadavg")
+	if err != nil {
+		log.Println(err)
+		return avgStat{}, err
+	}
+
+	load := *(*loadavg)(unsafe.Pointer((&b[0])))
+	scale := float64(load.scale)
+	ret := avgStat{
+		Load1:  float64(load.load[0]) / scale,
+		Load5:  float64(load.load[1]) / scale,
+		Load15: float64(load.load[2]) / scale,
+	}
+
+	return ret, nil
 }

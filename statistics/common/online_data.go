60b2a9ab9d683b7893b1a796243f6327c7847a69
nikitamakarchuk <kukushkanezabudka@yandex.by>
2022-04-05 08:37:13 +0300

    added check for cardano node

--- statistics/common/online_data.go
+++ statistics/common/online_data.go
@@ -1,6 +1,8 @@
 package common
 
 import (
+	"log"
+	"os/exec"
 	"time"
 
 	pb "github.com/adarocket/proto/proto-gen/common"
@@ -13,8 +15,18 @@ func (commonStatistic *CommonStatistic) GetOnlineData() *pb.Online {
 	onlineData.SinceStart = int64(time.Since(commonStatistic.startTime).Seconds())
 	onlineData.Pings = 0
 	// onlineData.ServerActive = true
-	onlineData.NodeActive = true
+	onlineData.NodeActive = isNodeActive()
 	onlineData.NodeActivePings = 0
 
 	return &onlineData
+}
+
+func isNodeActive() bool {
+	_, err := exec.Command("cardano-node", "version").Output()
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+
+	return true
 }
